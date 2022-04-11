833a3a92d34830bacd33ef1c6e15ded206e22e79
totoletoro57200 <33556892+totoletoro57200@users.noreply.github.com>
2022-04-11 16:21:48 +0200

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,43 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <h1>Bin2Dec</h1>
+    <input id="input" type="text" v-model="bin" @keyup="change">
+    <p v-if="result >= 0">Result: {{result}}</p>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+const regex = new RegExp("^[0-1]+$")
+
+export default {
+  name: "App",
+  data() {
+    return {
+      bin: "",
+      result: -1
+    };
+  },
+  methods: {
+    change() {
+      if(!regex.test(this.bin) && this.bin.length > 0) {
+        this.result = "Warning! Only 0 and 1 are allowed!";
+        return;
+      } else if(this.bin.length == 0) {
+        this.result = -1;
+      } else {
+        this.result = 0
+        for(let i = 0; i < this.bin.length; i++) {
+          this.result += parseInt(this.bin[i]) * Math.pow(2, this.bin.length - i -1);
+        }
+      }
+    }
+  }
 }
+</script>
+
+<style>
+  * {
+    font-family: sans-serif;
+    text-align: center;
+  }
 </style>
